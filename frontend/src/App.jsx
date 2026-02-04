59981b65bf830c547a5479e52afff3c2484484e0
Aaryan Kumar <kumaraaryan498@gmail.com>
2026-02-04 19:07:27 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Product from './pages/Product'
+import Order from './pages/Order'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<Order/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/product' element={<Product/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+      <Footer/>
+    
+    
+    
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,421 @@
+import logo from './logo.png'
+import search_icon from './search_icon.svg'
+import profile_icon from './profile_icon.svg'
+import cart_icon from './cart_icon.svg'
+import menu_icon from './menu_icon.svg'
+import back_icon from './back_icon.svg'
+import hero_img from './hero_img.jpg'
+import hero_img1 from './hero_img1.jpg'
+import exchange_icon from './exchange_icon.svg'
+import quality_icon from './quality_icon.svg'
+import support_icon from './support_icon.svg'
+
+
+import pic1 from './pic1.jpg'
+import pic2 from './pic2.jpg'
+import pic3 from './pic3.jpg'
+import pic4 from './pic4.jpg'
+import pic5 from './pic5.jpg'
+import pic6 from './pic6.jpg'
+import pic7 from './pic7.jpg'
+import pic8 from './pic8.jpg'
+import pic9 from './pic9.jpg'
+import pic10 from './pic10.jpg'
+import pic11 from './pic11.jpg'
+import pic12 from './pic12.jpg'
+import pic13 from './pic13.jpg'
+import pic14 from './pic14.jpg'
+import pic15 from './pic15.jpg'
+import pic16 from './pic16.jpg'
+import pic17 from './pic17.jpg'
+import pic18 from './pic18.jpg'
+import pic19 from './pic19.jpg'
+import pic20 from './pic20.jpg'
+import pic21 from './pic21.jpg'
+import pic22 from './pic22.jpg'
+import pic23 from './pic23.jpg'
+import pic24 from './pic24.jpg'
+import pic25 from './pic25.jpg'
+import pic26 from './pic26.jpg'
+import pic27 from './pic27.jpg'
+import pic28 from './pic28.jpg'
+import pic29 from './pic29.jpg'
+import pic30 from './pic30.jpg'
+
+export const assets={
+    logo,
+    search_icon,
+    profile_icon,
+    cart_icon,
+    menu_icon,
+    back_icon,
+    hero_img,
+    hero_img1,
+    exchange_icon,
+    quality_icon,
+    support_icon,
+}
+
+
+export const products=[
+    {
+        _id: "aaaaa01",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic1],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa02",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic2],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa03",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic3],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa04",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic4],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa05",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic5],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa06",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic6],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa07",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic7],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa08",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic8],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa09",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic9],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa10",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic10],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaa11",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic11],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa12",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic12],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa13",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic13],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa14",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic14],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa15",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic15],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa16",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic16],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa17",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic17],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa18",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic18],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa19",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic19],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa20",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic20],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa21",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic21],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa22",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic22],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa23",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic23],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa24",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic24],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa25",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic25],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa26",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic26],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa27",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic27],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa28",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic28],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa29",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic29],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaa30",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, ususally knitted, pullover shirt, close-fitting.",
+        price: 100,
+        image: [pic30],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+]
